fa4248065c40e306b45315972042b3f0bc43a026
Mammmmmm <33964815+Mammmmmm@users.noreply.github.com>
2018-03-05 23:44:27 +0000

    Update database.js

--- app/src/database.js
+++ app/src/database.js
@@ -1,46 +1,9 @@
-
-
-/*var Database = {
-	table: function (tableName) {
-		var table = Object.create(Table)
-		table.name = tableName
-		return table
-	}
-}
-*/
-/*
-var Table = {
-	name: '',
-	insert: function(record) {
-		var currentData = this.get()
-		currentData.record.push(record)
-		localStorage.setItem(this.name, JSON.stringify(currentData))
-	},
-	get: function () {
-		var a = localStorage.getItem(this.name)
-		if (a == null) {
-			return {record : []}
-		}
-		return JSON.parse(a)
-	},
-	remove: function (id) {
-		var currentData = this.get()
-		currentData.record.splice(id)
-		localStorage.setItem(this.name, JSON.stringify(currentData))
-	}
-}
-*/
-
-
-
 var Database = function (){
 
 }
 
 Database.prototype.table = function (tableName) {
-	//var table = Object.create(Table)
 	var table = new Table(tableName)
-	//table.name = tableName
 	return table
 }
 
@@ -49,7 +12,6 @@ Database.prototype.table = function (tableName) {
 
 
 var Table = function (tableName){
-	// constructor
 	this.name = tableName
 }
 
@@ -73,8 +35,6 @@ Table.prototype.remove = function (id) {
 	localStorage.setItem(this.name, JSON.stringify(currentData))
 }
 
-
-//Module = Module ? {} : Module
 if (typeof Module === 'undefined') {
 	Module = {}
 }
