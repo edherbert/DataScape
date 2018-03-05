1f02e14f5e04747656d35be9dd8a420b15dedac8
Mammmmmm <33964815+Mammmmmm@users.noreply.github.com>
2018-03-05 23:38:30 +0000

    update database.js

--- app/database.js
+++ app/database.js
@@ -1,30 +1,32 @@
 
 
-var Database = {
-	table: function (tableName) {
-		var table = Object.create(Table)
-		table.name = tableName
-		return table
-	}
+var Table = function (tableName){
+	this.name = tableName
 }
 
-var Table = {
-	name: '',
-	insert: function(record) {
-		var currentData = this.get()
-		currentData.record.push(record)
-		localStorage.setItem(this.name, JSON.stringify(currentData))
-	},
-	get: function () {
-		var a = localStorage.getItem(this.name)
-		if (a == null) {
-			return {record : []}
-		}
-		return JSON.parse(a)
-	},
-	remove: function (id) {
-		var currentData = this.get()
-		currentData.record.splice(id)
-		localStorage.setItem(this.name, JSON.stringify(currentData))
-	}
+Table.prototype.insert = function (record) {
+	var currentData = this.get()
+	currentData.record.push(record)
+	localStorage.setItem(this.name, JSON.stringify(currentData))
 }
+
+Table.prototype.get = function () {
+	var a = localStorage.getItem(this.name)
+	if (a == null) {
+		return {record : []}
+	}
+	return JSON.parse(a)
+}
+
+Table.prototype.remove = function (id) {
+	var currentData = this.get()
+	currentData.record.splice(id)
+	localStorage.setItem(this.name, JSON.stringify(currentData))
+}
+
+
+
+if (typeof Module === 'undefined') {
+	Module = {}
+}
+Module.exports = new Database
