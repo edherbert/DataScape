f95854791b8605a30988241a1b1c7db575409950
Rmcc2147 <34538968+Rmcc2147@users.noreply.github.com>
2018-03-09 12:36:27 +0000

    Created EmailType
    Created EmailType dependant on FirstNameType and SecondNameType

---
+++ app/src/TypeManager/types/EmailType.js
@@ -0,0 +1,27 @@
+const FieldType = require("../FieldType");
+
+function EmailType(){
+	this.emailService = ['@gmail', '@yahoo', '@hotmail', '@outlook'];
+  this.emailEnding = ['.com', '.co.uk'];
+}
+
+EmailType.prototype = Object.assign(Object.create(FieldType.prototype), {
+  constructor: EmailType,
+
+  Requirements: ["FirstName"], ["SecondName"],
+
+  generate: function(previous){
+    let current = previous;
+    if(typeof previous == 'undefined') current = {};
+
+    let targetService = "";
+		let targetEnding = "";
+    targetService = this.emailService[Math.floor(Math.random() * this.emailService.length)];
+    targetEnding = this.emailEnding[Math.floor(Math.random() * this.emailEnding.length)];
+    current.Email = previous.FirstName ++ previous.SecondName ++ this.emailService ++ this.emailEnding;
+
+    return current;
+  }
+});
+
+module.exports = EmailType;
