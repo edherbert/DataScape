df5ce712a7c4ed3c90ddad033651e1b4432ee968
Rmcc2147 <34538968+Rmcc2147@users.noreply.github.com>
2018-03-09 14:47:36 +0000

    Data type bug fixed
    Fixed bug where an int was attempted to be set to 'target' while target was of type String.

--- app/src/TypeManager/types/PhoneNumberType.js
+++ app/src/TypeManager/types/PhoneNumberType.js
@@ -11,8 +11,8 @@ PhoneNumberType.prototype = Object.assign(Object.create(FieldType.prototype), {
     if(typeof previous == 'undefined') current = {};
 
     let target = "";
-    target = Math.floor(Math.random() * 1000000000) + 9999999999
-    current.PhoneNumber = "+44" + String(target);
+    target = String(Math.floor(Math.random() * 1000000000) + 9999999999)
+    current.PhoneNumber = "+44" + target;
 
     return current;
   }
