d22c3f69435103386077d071f3a4b5ff53469a86
Chun Lok Li <up758327@myport.ac.uk>
2018-03-09 18:18:44 +0000

    Add files via upload

---
+++ app/src/TypeManager/types/BornCountry.js
@@ -0,0 +1,22 @@
+const FieldType = require("../FieldType");
+
+function CountryType(){
+  this.country = ['England', 'Scoland', 'Ireland', 'Poland', 'India', 'Pakistan', 'Germany', 'South Africa', 'United State', 'Australia', 'China'];
+}
+
+CountryType.prototype = Object.assign(Object.create(FieldType.prototype), {
+  constructor: CountryType,
+
+  generate: function(previous){
+    let current = previous;
+    if(typeof previous == 'undefined') current = {};
+
+    let target = "";
+    target = this.country[Math.floor(Math.random() * this.country.length)]
+    current.country = target;
+
+    return current;
+  }
+});
+
+module.exports = CountryType;
