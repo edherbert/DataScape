10f2e790bfdd7fc0281af33aa2779405ef4665f7
edherbert <edwardherbert802@yahoo.com>
2018-03-09 15:54:59 +0000

    Fixed a bug with the postcode type

--- app/src/TypeManager/types/PostcodeType.js
+++ app/src/TypeManager/types/PostcodeType.js
@@ -14,10 +14,10 @@ PostcodeType.prototype = Object.assign(Object.create(FieldType.prototype), {
     if(typeof previous == 'undefined') current = {};
 
     let city = previous.City;
-    let cityCode = cityCode = city.substring(0,2).toUpperCase();
+    let cityCode = city.substring(0,2).toUpperCase();
     let randChar = this.characters[Math.floor(Math.random() * this.characters.length)];
     let randChars = this.randChar + this.randChar;
-    let randNum = String(Maths.floor(Math.random() * 9))
+    let randNum = String(Math.floor(Math.random() * 9))
 
     current.Postcode = this.cityCode + this.randNum + " " + this.randNum + this.randChars;
     return current;
