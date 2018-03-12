a6e5fc0863c5b63f1bfc860f2f8a521d41b1be39
Rmcc2147 <34538968+Rmcc2147@users.noreply.github.com>
2018-03-12 15:38:41 +0000

    Revert "Merge branch 'master' of https://github.com/edherbert/DataScape"
    This reverts commit 4f0bc321bfad1a27259f5461706ea9daac2dbfcd, reversing
    changes made to 2e79746bee944892e992ebbcba6804b64db06805.

--- app/src/pages/DatabaseCreationPopup.js
+++ app/src/pages/DatabaseCreationPopup.js
@@ -27,22 +27,15 @@ DatabaseCreationPopup.prototype = Object.assign(Object.create(Popup.prototype), 
 
     let declineDbButton = document.createElement('div');
     declineDbButton.id = "declineDbButton";
-    declineDbButton.innerHTML = "Cancel";
+    declineDbButton.innerHTML = "Decline";
 
     let that = this;
     acceptDbButton.onclick = function(e){
-      if (that.databaseTitleInput.value != ""){
-        that.dbSelectionView.addToList(that.databaseTitleInput.value);
-        storageManager.createDatabase(that.databaseTitleInput.value);
-        that.popout();
-      }else{
-        that.popout();
-      }
-    }
-
-    declineDbButton.onclick = function(e){
+      that.dbSelectionView.addToList(that.databaseTitleInput.value);
+      storageManager.createDatabase(that.databaseTitleInput.value);
       that.popout();
     }
+
 
     this.backgroundView.append(databaseTitle);
     this.backgroundView.append(this.databaseTitleInput);
